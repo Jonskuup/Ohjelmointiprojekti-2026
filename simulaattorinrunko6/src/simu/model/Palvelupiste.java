2b3d3a30e5b7c68c53c059dae27d6c8983c75cd2
ivarih <ivarih@metropolia.fi>
2026-09-21 19:08:54 +0300

    Tilastointimuutokset, tilastojen päivitys "aloitaPalvelu"-metodilla ja getterit raportointia varten

--- simulaattorinrunko6/src/simu/model/Palvelupiste.java
+++ simulaattorinrunko6/src/simu/model/Palvelupiste.java
@@ -12,51 +12,61 @@ public class Palvelupiste {
 	private final ContinuousGenerator generator;
 	private final Tapahtumalista tapahtumalista;
 	private final TapahtumanTyyppi skeduloitavanTapahtumanTyyppi;
-	
+
 	//JonoStartegia strategia; //optio: asiakkaiden järjestys
-	
+
 	private boolean varattu = false;
+	private int palvellutAsiakkaat = 0;
+	private double aktiivinenAika = 0;
 
-
-	public Palvelupiste(ContinuousGenerator generator, Tapahtumalista tapahtumalista, TapahtumanTyyppi tyyppi){
+	public Palvelupiste(ContinuousGenerator generator, Tapahtumalista tapahtumalista, TapahtumanTyyppi tyyppi) {
 		this.tapahtumalista = tapahtumalista;
 		this.generator = generator;
 		this.skeduloitavanTapahtumanTyyppi = tyyppi;
-				
+
 	}
 
 
-	public void lisaaJonoon(Asiakas a){   // Jonon 1. asiakas aina palvelussa
+	public void lisaaJonoon(Asiakas a) {   // Jonon 1. asiakas aina palvelussa
 		jono.add(a);
-		
+
 	}
 
 
-	public Asiakas otaJonosta(){  // Poistetaan palvelussa ollut
+	public Asiakas otaJonosta() {  // Poistetaan palvelussa ollut
 		varattu = false;
 		return jono.poll();
 	}
 
 
-	public void aloitaPalvelu(){  //Aloitetaan uusi palvelu, asiakas on jonossa palvelun aikana
-		
+	public void aloitaPalvelu() {  //Aloitetaan uusi palvelu, asiakas on jonossa palvelun aikana
+
 		Trace.out(Trace.Level.INFO, "Aloitetaan uusi palvelu asiakkaalle " + jono.peek().getId());
-		
+
 		varattu = true;
 		double palveluaika = generator.sample();
-		tapahtumalista.lisaa(new Tapahtuma(skeduloitavanTapahtumanTyyppi,Kello.getInstance().getAika()+palveluaika));
+		aktiivinenAika += palveluaika;
+		palvellutAsiakkaat++;
+		tapahtumalista.lisaa(new Tapahtuma(skeduloitavanTapahtumanTyyppi, Kello.getInstance().getAika() + palveluaika));
 	}
 
 
-
-	public boolean onVarattu(){
+	public boolean onVarattu() {
 		return varattu;
 	}
 
 
-
-	public boolean onJonossa(){
+	public boolean onJonossa() {
 		return jono.size() != 0;
 	}
 
+
+	public int getPalvellutAsiakkaat() {
+		return palvellutAsiakkaat;
+	}
+
+	public double getAktiivinenAika() {
+		return aktiivinenAika;
+	}
 }
+
